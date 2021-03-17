2a0adfc120d5a4dc0a64239169155342ede4237f
Casper Pedersen <ctp@logos.dk>
2021-03-17 10:00:21 +0100

    Working on DDR mapping

--- include/configs/logosni8.h
+++ include/configs/logosni8.h
@@ -8,11 +8,14 @@
 #ifndef __LOGOS_NI8_H__
 #define __LOGOS_NI8_H__
 
-#define CONFIG_MXC_UART_BASE            UART5_BASE
+#define CONFIG_MXC_UART_BASE            UART4_BASE
 
 #include"mx6_common.h"
 
-#define CONFIG_NR_DRAM_BANKS            1
+/* TODO Is set to 1 for all other iMX6 boards.
+ * Our SDRAM is listed with 8 banks in the datasheet, so should it be 3 (2**3=8)?
+ */
+//#define CONFIG_NR_DRAM_BANKS            1
 #define CONFIG_SYS_MAX_FLASH_BANKS      1
 #define CONFIG_SYS_MALLOC_LEN           (10 * SZ_1M)
 #define CONFIG_SYS_FSL_ESDHC_ADDR       0
@@ -22,7 +25,6 @@
 #define CONFIG_SYS_INIT_RAM_SIZE        IRAM_SIZE
 #define CONFIG_SYS_INIT_SP_OFFSET \
   (CONFIG_SYS_INIT_RAM_SIZE - GENERATED_GBL_DATA_SIZE)
-
 #define CONFIG_SYS_INIT_SP_ADDR \
   (CONFIG_SYS_INIT_RAM_ADDR + CONFIG_SYS_INIT_SP_OFFSET)
 #endif
