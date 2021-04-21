82089e5f0ba058bf6037b5f34f3a05ec4b44498c
Casper Pedersen <ctp@logos.dk>
2021-04-21 09:53:11 +0200

    Minor adjustements to logo.

--- board/logos/ni8/logosLogo.h
+++ board/logos/ni8/logosLogo.h
@@ -6,8 +6,10 @@
 #ifndef LOGOSLOGO_H
 #define LOGOSLOGO_H
 
+#define LOGOS_LOGO_ROWS 26
+#define LOGOS_LOGO_COLS 201
 //const char logosLogo[8][80] =
-const char logosLogo[26][200] =
+const char logosLogo[LOGOS_LOGO_ROWS][LOGOS_LOGO_COLS] =
 {
 /*
 	{"....q#P....Pqu.................................................................."},
@@ -19,32 +21,32 @@ const char logosLogo[26][200] =
 	{"..#c.Peuqqo#P.sP......)######b...P#QQQ#PC......P#QQQ#PP....)P#QQQ#PC...P#QQQ##P."},
 	{".....#qq.uqoP..................................................................."}
 */
-{"...............qQQQQQQQQQQQQpu.........................................................................................................................................................................."},
-{"..........qQQ#PPC..........()P#QQp......................................................................................................................................................................"},
-{".......qQQP.......qqpppppq......)SQQp..................................................................................................................................................................."},
-{".....qQ#C.....#Q#PPCCC(C)P#QQQu....)#Qb.................QQQQQQQ.....................qQQQQQQQQQQQQQQp...................qQQQQQQQQQQQQQQpu..............qpQQQQQQQQQQQQQQp..............pQQQQQQQQQQQQQpu..."},
-{"....Q#C....qQ#P..............)#Qb....)QQu...............QQQQQQQ..................QQQQQQQQQQQQQQQQQQQQQb.............dQQQQQQQQQQQQQQQQQQQQQu........qQQQQQQQQQQQQQQQQQQQQQp.........QQQQQQQQQQQQQQQQQQQQC"},
-{"..(QP.....Q#C....qQQ#PP#QQbc...)QQu....)Qb..............QQQQQQQ................QQQQQQQQQQQ###QQQQQQQQQQQQ.........#QQQQQQQQQQ####QQQQQQQ#........sQQQQQQQQQQ####QQQQQQQQQQQp.....)QQQQQQQQ#PP##QQQQQQ#.."},
-{".)QP....sQ#....dQPC.......#Qb....)Qb....)Qb.............QQQQQQQ..............)QQQQQQQQPC........)#QQQQQQQQu......QQQQQQQQP..........)#PC........#QQQQQQQ#P.........)#QQQQQQQQ....QQQQQQQP.........)#P..."},
-{".QQ....#QP....#QP....QQu...)QQ....)QQ....#QC............QQQQQQQ.............)QQQQQQQP..............#QQQQQQQ.....QQQQQQQ#.......................(QQQQQQQC.............)QQQQQQQQ...#QQQQQQQc.............."},
-{"(QC...#QP....QQC...dQP)Qb....QQu...)QQ...)QQ............QQQQQQQ.............QQQQQQQP................#QQQQQQQ...#QQQQQQ#........qqqqqqqqqqqqc...QQQQQQQC................QQQQQQQC..)QQQQQQQQQQQpq........."},
-{"QQ....Q#...)Q#....#QP..)QQ....#Qp...(QC...QQ............QQQQQQQ.............QQQQQQQ.................)QQQQQQQ...QQQQQQQC........QQQQQQQQQQQQC..)QQQQQQQ.................(QQQQQQQ...)QQQQQQQQQQQQQQQQp...."},
-{"#Q...)Qb...QQ....QQC.....QQu...#Q...)Qb...QQ............QQQQQQQ.............QQQQQQQ.................)QQQQQQQ...#QQQQQQC........QQQQQQQQQQQQC..)QQQQQQQ.................(QQQQQQ#......)#QQQQQQQQQQQQQQQu."},
-{")QC...QQ...QQ....)........P....QQ...(QC...Q#............QQQQQQQ.............QQQQQQQb................#QQQQQQ#...(QQQQQQQ........PPPPPPPPPSQQC...QQQQQQQC................QQQQQQQC............P#QQQQQQQQQQb"},
-{".QQ...#QC...QQu...............QQC...Q#...#QC............QQQQQQQ.............)QQQQQQQb..............#QQQQQQQ.....QQQQQQQQ...............sQQQC...)QQQQQQQu..............QQQQQQQ#.................)QQQQQQQQ"},
-{".)Qb...#Qp...)QQQppppppppppQQ#P...)Q#...(QP.............QQQQQQQu.............)QQQQQQQQpc........s#QQQQQQQQC......QQQQQQQQQu.........spQQQQQC....#QQQQQQQQp.........qQQQQQQQQ#.....sQpq..........#QQQQQQQ"},
-{"..)QQ...)QQu.....).........).....QQP...#QP..............QQQQQQQQQQQQQQQQQQ.....QQQQQQQQQQQQQQQQQQQQQQQQQP.........#QQQQQQQQQQQQQQQQQQQQQQQQC.....)QQQQQQQQQQQQQQQQQQQQQQQQQP.....#QQQQQQQQQQQQQQQQQQQQQC"},
-{"...)QQu...)QQbq..............qpQ#P....QQC...............QQQQQQQQQQQQQQQQQQ......)#QQQQQQQQQQQQQQQQQQQQPC............SQQQQQQQQQQQQQQQQQQQQ#C........)QQQQQQQQQQQQQQQQQQQQ#P......QQQQQQQQQQQQQQQQQQQQQ#C."},
-{".....)QQu....)P#QQQQQQQQQQQQ#PP.....QQP.................QQQQQQQQQQQQQQQQQQ..........P#QQQQQQQQQQQQ#PC.................(P#QQQQQQQQQQQQ#PC..............)#QQQQQQQQQQQQ#PP............P#QQQQQQQQQQQQQ#P...."},
-{".......)QQbc.....................qQ#P....................................................(CCCC).............................()CCC)..........................(CCCC........................()CCC)........."},
-{"..........)#QQpq............qpQQ#P......................................................................................................................................................................"},
-{"...............PP#QQQQQQQ##PPC.........................................................................................................................................................................."},
-{"........................................................qqqqqp...............................................................p.....p.................)p................................................."},
-{"........................................................Q....................................................................Q.....Q...................................................................."},
-{"........................................................Qpppp..#P..#QC))b..QP..Sb..#CC)Qu.Q..)#b..#C.QC()Qu.Q#PS.#C()Q..QPC.QQ.....Q..#P.)Sb..QPC)Q#.)Q..QPC)Qu........................................."},
-{"........................................................Q......#C..)C...Q..Q....QC#C...)Q.)b.#.)C)P.(PPPPPP.QC..#PPPPPP)Q....Q.....Q..Q....#C)b...)#.)Q.#C.............................................."},
-{"........................................................Qppppp.#C..)C...Q..Q#qqQP..#pqq#C..#Q...#Q...#pqq#..QC...#pqq#..#bqqbQ.....Q..)Qqq#P..)##P)#.)Q..#pqq#.........................................."},
-{"...........................................................................Q..................................................................Qu..#C...................................................."}
+{"               qDesign_byQQQpu                                                                                                                                                                          \0"},
+{"          qQQ#PPC          ()HKHQp                                                                                                                                                                      \0"},
+{"       qQQP       qqpppppq      )LMBp                                                                                                                                                                   \0"},
+{"     qQ#C     #Q#PPCCC(C)P#QQQu    CTPb                 QQQQQQQ                     qQQQQQQQQQQQQQQp                   qQQQQQQQQQQQQQQpu              qpQQQQQQQQQQQQQQp              pQQQQQQQQQQQQQpu   \0"},
+{"    Q#C    qQ#P              )#Qb    )QQu               QQQQQQQ                  QQQQQQQQQQQQQQQQQQQQQb             dQQQQQQQQQQQQQQQQQQQQQu        qQQQQQQQQQQQQQQQQQQQQQp         QQQQQQQQQQQQQQQQQQQQC\0"},
+{"  (QP     Q#C    qQQ#PP#QQbc   )QQu    )Qb              QQQQQQQ                QQQQQQQQQQQ###QQQQQQQQQQQQ         #QQQQQQQQQQ####QQQQQQQ#        sQQQQQQQQQQ####QQQQQQQQQQQp     )QQQQQQQQ#PP##QQQQQQ#  \0"},
+{" )QP    sQ#    dQPC       #Qb    )Qb    )Qb             QQQQQQQ              )QQQQQQQQPC        )#QQQQQQQQu      QQQQQQQQP          )#PC        #QQQQQQQ#P         )#QQQQQQQQ    QQQQQQQP         )#P   \0"},
+{" QQ    #QP    #QP    QQu   )QQ    )QQ    #QC            QQQQQQQ             )QQQQQQQP              #QQQQQQQ     QQQQQQQ#                       (QQQQQQQC             )QQQQQQQQ   #QQQQQQQc              \0"},
+{"(QC   #QP    QQC   dQP)Qb    QQu   )QQ   )QQ            QQQQQQQ             QQQQQQQP                #QQQQQQQ   #QQQQQQ#        qqqqqqqqqqqqc   QQQQQQQC                QQQQQQQC  )QQQQQQQQQQQpq         \0"},
+{"QQ    Q#   )Q#    #QP  )QQ    #Qp   (QC   QQ            QQQQQQQ             QQQQQQQ                 )QQQQQQQ   QQQQQQQC        QQQQQQQQQQQQC  )QQQQQQQ                 (QQQQQQQ   )QQQQQQQQQQQQQQQQp    \0"},
+{"#Q   )Qb   QQ    QQC     QQu   #Q   )Qb   QQ            QQQQQQQ             QQQQQQQ                 )QQQQQQQ   #QQQQQQC        QQQQQQQQQQQQC  )QQQQQQQ                 (QQQQQQ#      )#QQQQQQQQQQQQQQQu \0"},
+{")QC   QQ   QQ    )        P    QQ   (QC   Q#            QQQQQQQ             QQQQQQQb                #QQQQQQ#   (QQQQQQQ        PPPPPPPPPSQQC   QQQQQQQC                QQQQQQQC            P#QQQQQQQQQQb\0"},
+{" QQ   #QC   QQu               QQC   Q#   #QC            QQQQQQQ             )QQQQQQQb              #QQQQQQQ     QQQQQQQQ               sQQQC   )QQQQQQQu              QQQQQQQ#                 )QQQQQQQQ\0"},
+{" )Qb   #Qp   )QQQppppppppppQQ#P   )Q#   (QP             QQQQQQQ              )QQQQQQQQpc        s#QQQQQQQQC      QQQQQQQQQu         spQQQQQC    #QQQQQQQQp         qQQQQQQQQ#     sQpq          #QQQQQQQ\0"},
+{"  )QQ   )QQu     )         )     QQP   #QP              QQQQQQQQQQQQQQQQQQ     QQQQQQQQQQQQQQQQQQQQQQQQQP         #QQQQQQQQQQQQQQQQQQQQQQQQC     )QQQQQQQQQQQQQQQQQQQQQQQQQP     #QQQQQQQQQQQQQQQQQQQQQC\0"},
+{"   )QQu   )QQbq              qpQ#P    QQC               QQQQQQQQQQQQQQQQQQ      )#QQQQQQQQQQQQQQQQQQQQPC            SQQQQQQQQQQQQQQQQQQQQ#C        )QQQQQQQQQQQQQQQQQQQQ#P      QQQQQQQQQQQQQQQQQQQQQ#C \0"},
+{"     )QQu    )P#QQQQQQQQQQQQ#PP     QQP                 QQQQQQQQQQQQQQQQQQ          P#QQQQQQQQQQQQ#PC                 (P#QQQQQQQQQQQQ#PC              )#QQQQQQQQQQQQ#PP            P#QQQQQQQQQQQQQ#P    \0"},
+{"       )QQbc                     qQ#P                                                    (CCCC)                             ()CCC)                          (CCCC                        ()CCC)         \0"},
+{"          )#QQpq            qpQQ#P                                                                                                                                                                      \0"},
+{"               PP#QQQQQQQ##PPC                                                                                                                                                                          \0"},
+{"                                                        ######                                                               #     #                 ##                                                 \0"},
+{"                                                        #                                                                    #     #                                                                    \0"},
+{"                                                        #####  ##########  ######  #####  #   ##  #  ###### #### #####  ######     #   #####  ###### ##  #####                                          \0"},
+{"                                                        #      ##  ##   #  #    # ##   ##  # #  # # ## # ## ##  ## # ## #    #     #  ##   ## #   ## ## ##                                              \0"},
+{"                                                        ###### ##  ##   #  ######  #####   ##   ##   #####  ##   #####  ######     #   #####  ###### ##  #####                                          \0"},
+{"                                                                           #                                                                  ##  ##                                                    \0"}
  };
 
 #endif //LOGOSLOGO_H
