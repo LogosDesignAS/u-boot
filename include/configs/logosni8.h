81c9be2c75b19f57f6198fd53b14638eed994772
hkh <hkh@pop-os.localdomain>
2021-06-15 15:28:03 +0200

    Fram can be written to and also hold the data after the bard has been reset. Commented out the divice tree in configuration to make it possible to run the code with jtag.

--- include/configs/logosni8.h
+++ include/configs/logosni8.h
@@ -50,6 +50,7 @@
 #define CONFIG_SYS_I2C_MXC_I2C1		/* enable I2C bus 1 */
 #define CONFIG_SYS_I2C_MXC_I2C2		/* enable I2C bus 2 */
 #define CONFIG_SYS_I2C_MXC_I2C3		/* enable I2C bus 3 */
+#define CONFIG_SYS_I2C_MXC_I2C4		/* enable I2C bus 4 */
 #define CONFIG_SYS_I2C_SPEED		100000
 #define CONFIG_I2C_EDID
 
