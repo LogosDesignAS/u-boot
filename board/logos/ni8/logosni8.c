caa213a6717598d554efa6e66ac8a6ea7c1035dc
LucasBallingLMB <lmb@logos.dk>
2022-01-10 14:17:05 +0100

    Ficed some warnings for U-boot.imx

--- board/logos/ni8/logosni8.c
+++ board/logos/ni8/logosni8.c
@@ -266,10 +266,12 @@ static iomux_v3_cfg_t const uart2_pads[] = {
 };
 
 /* Configuration of UART4 GPIO Pins for Logosni8 Testing boot time */
+#ifdef CONFIG_SPL_BUILD
 static iomux_v3_cfg_t const uart4_gpio_pads[] = {
 		IOMUX_PAD_CTRL(CSI0_DAT12__GPIO5_IO30, NO_PAD_CTRL),
 		IOMUX_PAD_CTRL(CSI0_DAT13__GPIO5_IO31, NO_PAD_CTRL),
 };
+#endif // CONFIG_SPL_BUILD
 
 /* Configuration of UART4 for Logosni8 */
 static iomux_v3_cfg_t const uart4_pads[] = {
